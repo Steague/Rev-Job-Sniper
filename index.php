eda22d23e860569d89f6f004829eb3be76130d82
zentits <zentits@ps25657.dreamhostps.com>
2016-03-04 11:28:31 -0800

    Adding socket io for push notifications

--- index.php
+++ index.php
@@ -164,10 +164,17 @@ h1 {
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.5/socket.io.min.js"></script>
     <script>
+    var socket;
     $(document).ready(function() {
         initAPI();
         initButtonEvents();
+        socket = io.connect('zentits.com:8000');
+
+        socket.on('notification', function(data) {
+            console.log("socket io got a notification!", data);
+        });
     });
 
     function initButtonEvents() {
